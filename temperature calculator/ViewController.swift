0f2ade97a8d07d34d112f8189ed4ef2d75269b88
OskarNiemira <oskar.niemira@gmail.com>
2017-06-17 16:20:50 +0200

    added all of the variables

--- temperature calculator/ViewController.swift
+++ temperature calculator/ViewController.swift
@@ -5,11 +5,21 @@
 //  Created by Oskar on 14.06.2017.
 //  Copyright © 2017 Oskar Niemira. All rights reserved.
 //
+// simple app which exchanges temperature
+
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var label1: UILabel!
+    @IBOutlet var label2: UILabel!
+    @IBOutlet var imgeView: UIImageView!
+    @IBOutlet var segment: UISegmentedControl!
+    @IBOutlet var textField: UITextField!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+    
+    @IBAction func segment(_ sender: Any) {
+    }
+    
 
 }
 
